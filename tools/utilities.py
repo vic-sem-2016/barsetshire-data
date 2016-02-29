fbbbb7c7d1745a14b70f5349d0643800881a298a
Nabil Kashyap <n@nabilk.com>
2016-02-29 14:20:24 -0500

    turned scripts into (crummy) functions for reuse

--- tools/utilities.py
+++ tools/utilities.py
@@ -3,47 +3,72 @@
 # A grab bag of scripts and ideas of possible use for basic text mining
 # using NLTK
 
-import os, re, nltk
+import os, re, nltk, json
 from sys import argv
 
-with open(path_in) as file:
-    text = file.read()
+    # with open(path_in) as file:
+    # text = file.read()
 
-    # tokenize / break string into meaningful units, in this case words
+    # # tokenize / break string into meaningful units, in this case words
 
-    tokens = nltk.word_tokenize(text)
+    # tokens = nltk.word_tokenize(text)
 
-    # this separates out punctuation
-    tokens = nltk.WordPunctTokenizer().tokenize(s)
+    # # this separates out punctuation
+    # tokens = nltk.WordPunctTokenizer().tokenize(s)
 
-    # this pattern strips punctuation
-    tokens = nltk.regexp_tokenizer(text, pattern=r'\w+')
+    # # this pattern strips punctuation
+    # tokens = nltk.regexp_tokenizer(text, pattern=r'\w+')
 
-    # probably useful helper functions from os library
-    # for parsing files/folders
+    # # probably useful helper functions from os library
+    # # for parsing files/folders
 
-    os.listdir():
-    os.mkdir()
-    os.getcwd()
+    # os.listdir():
+    # os.mkdir()
+    # os.getcwd()
 
-    # frequency distributions
+    # # frequency distributions
 
-    fdist1 = nltk.FreqDist(tokens)
-    top_used = fdist1.most_common(500)
-    json.dumps()
+    # fdist1 = nltk.FreqDist(tokens)
+    # top_used = fdist1.most_common(500)
+    # json.dumps()
 
+def get_most_common_novels(path_in,num):
+# gets most common words from texts and writes out
+# to json files, according to path_in and number
+# of common words to extract
 
-for i in os.listdir('.'):
-    with open(i) as f:
-        text = f.read()
-        tokens = nltk.word_tokenize(text)
-        fdist = nltk.FreqDist(tokens)
-        most_used = fdist.most_common(500)
-        path_out = "../freqs/"
-        path_out += str.split(i,'.')[0]
-        path_out += '.json'
-        out = open(path_out,'w')
-        out.write(json.dumps(most_used))
-        out.close()
+    for i in os.listdir(path_in):
+        with open(path_in + '/' + i) as f:
+            text = f.read()
+            tokens = nltk.word_tokenize(text)
+            fdist = nltk.FreqDist(tokens)
+            most_used = fdist.most_common(int(num))
+            path_out = str.split(i,'.')[0]
+            path_out += '.json'
+            out = open(path_out,'w')
+            out.write(json.dumps(most_used))
+            out.close()
 
+def get_most_common_combined(path_in,num):
+# gets most common words from combining texts and writes out
+# to json files, according to path_in and number of most
+# common words to extract
 
+    fulltext = ""
+    for i in os.listdir(path_in):
+      with open(path_in + '/' + i) as f:
+        curr_vol = f.read()
+        fulltext += " " + curr_vol
+    tokens = nltk.word_tokenize(fulltext)
+    fdist = nltk.FreqDist(tokens)
+    most_used = fdist.most_common(int(num))
+    out = open('combined-tokens.json','w')
+    out.write(json.dumps(most_used))
+    out.close()
+
+if __name__ == '__main__':
+
+    # takes two arguments, the filename and the number of most common
+    # words
+    # get_most_common_combined(argv[1],argv[2])
+    get_most_common_novels(argv[1],argv[2])
